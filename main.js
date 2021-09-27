8bf90bfd302c49a52b78d6b22f8cf2bd4e76d702
ldh-30510 <85325287+ldh-30510@users.noreply.github.com>
2021-09-27 10:08:58 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,71 @@
-var sign = function() {
-    var id = document.getElementById('id').value;
-    var password = document.getElementById('password').value;
+/*
+function login() {
+    const form = document.login_form;
+    const chkUsername = checkValidUsername(form);
+    const chkPassword = checkValidPassword(form);
 
-    if(id==="pi"&&password==="1234"){
-        source();
+    if(chkUsername){
+        document.getElementById('alert_username').innerText = "";
+    } else {
+        
+    }
     }
 }
 
-var source = function () {
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+window.onload = function () {
+    var id = document.getElementById("id");
+    var password = document.getElementById("password");
+
     var btn = document.getElementById("btn");
     var link = "https://naver.com";
+
+    
     btn.onclick = function(){
-        location.replace(link);
+        if(id == "pi" && password == "1234"){
+            location.replace(link);
+        }
+    btn.onclick()
     }
 }
+*/
+/*
+function check(form)
+{
+    if(form.id.value=="pi" && form.password.value=="1234")
+    {
+        window.open('https://naver.com')
+        /*
+        window.onload = function() {
+            var btn = document.getElementById("btn");
+            var link = "https://naver.com";
+
+            btn.onclick = function(){
+                location.replace(link);
+            }
+        btn.onclick()
+        }
+        
+    }
+    else
+    {
+        alert("Error USER NAME or PASSWORD")
+    }
+
+}
+*/
